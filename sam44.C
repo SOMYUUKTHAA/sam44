aaad3873004f138f736bd46c27cc43d1c367d149
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-10 10:55:40 +0000

    Create sam44.C

---
+++ sam44.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,a=1;
+    printf("ENTER A NUMBER\t");
+    scanf("%d",&n);
+    for(a=1;a<=10;a++)
+    {
+    if(n==a)
+    {
+        printf("YES,IT IS IN RANGE");
+    }
+}
+}
